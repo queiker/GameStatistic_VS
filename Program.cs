5febd119de771f7b7257b16b2b772f0b9b5b7707
Grzegorz Płonka <tradecomp.pl@gmail.com>
2021-02-21 10:14:27 +0100

    begining

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using System;
+
+
+namespace GameStatistic_VS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            string text =
+            "TEST OK";
+            write_to_file(text, "WriteText.txt");
+
+        }
+
+        static void write_to_file(string line, string src_to_file)
+        {
+            File.WriteAllTextAsync(src_to_file, line);
+        }
+
+
+
+
+
+
+
+    }
+}
